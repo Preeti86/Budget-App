4cfc6406939857ef45cdb33a81ac7c957df40e2c
preeti86 <preetijav@gmail.com>
2019-04-14 23:56:24 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// Budget Controller
+
+var budgetController = (function () {
+
+})();
+
+
+// UI controller
+var UIController = (function () {
+
+    // Some code
+
+})();
+
+
+
+// Global app controller
+var controller = (function (budgetCtrl, UICtrl) {
+
+
+})(budgetController, UIController);
+
